7031bbb837d258c56459f75b780d99aeebef8cc1
rusconn <rusconn3@gmail.com>
2024-09-13 13:07:13 +0900

    fix: handle HTTPException

--- api/src/errorhandlers/onError.ts
+++ api/src/errorhandlers/onError.ts
@@ -1,10 +1,17 @@
 import type { ErrorHandler } from "hono";
+import { HTTPException } from "hono/http-exception";
 
 import { logger as log } from "../logger.ts";
 import type { Env } from "../types.ts";
 
 export const onError: ErrorHandler<Env> = (err, c) => {
   const { requestId } = c.var;
+
   log.error({ requestId, err }, "error-log");
+
+  if (err instanceof HTTPException) {
+    return err.getResponse();
+  }
+
   return c.json("Internal Server Error", 500);
 };
