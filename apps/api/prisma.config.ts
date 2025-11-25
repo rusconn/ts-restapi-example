2d2632e770be02ad320a86d66b83a5ef452ea29a
rusconn <rusconn3@gmail.com>
2025-11-25 11:10:31 +0900

    chore(deps): upgrade prisma

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,11 +1,14 @@
 import path from "node:path";
 
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: path.join("prisma", "schema"),
   migrations: {
     path: path.join("prisma", "schema", "migrations"),
     seed: `node ${path.join("prisma", "seed.ts")}`,
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
   },
 });
